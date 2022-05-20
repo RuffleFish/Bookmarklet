71fe36c11518908910ba02ddf58180d373daff87
RuffleFish <s2006146@ed.ac.uk>
2022-05-20 22:27:32 +0200

    bookmarklet edited

---
+++ bookmarklet.js
@@ -0,0 +1,11 @@
+(function(){
+  console.log("bookmarklet starting");
+
+})();
+
+(function(){
+  letscript = document.createElement('script');
+  script.src = 'bookmarklet.js';
+  document.body.appendChild(script);
+
+})();
