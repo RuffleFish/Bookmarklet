1fc2bf0bef8e2eaf1dda22374a6135c067a66fa9
RuffleFish <s2006146@ed.ac.uk>
2022-05-23 17:28:13 +0200

    bookmarklet update

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,9 +3,9 @@
 
 })();
 
-(function(){
-  letscript = document.createElement('script');
-  script.src = 'bookmarklet.js';
-  document.body.appendChild(script);
-
-})();
+// (function(){
+//   letscript = document.createElement('script');
+//   script.src = 'https://cdn.jsdelivr.net/gh/RuffleFish/Bookmarklet@master/bookmarklet.js';
+//   document.body.appendChild(script);
+//
+// })();
